e5dbc68f93032767342c9f1946f3b161c95443cb
Paul Gilzow <paul.gilzow@platform.sh>
2021-12-15 11:13:19 -0600

    that's right, we dont have sudo. removed

--- scripts/autoupdate.sh
+++ scripts/autoupdate.sh
@@ -4,10 +4,10 @@ set -e
 echo "beginning auto-update check"
 echo "Can we install the GH cli tool?"
 
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update
+apt install gh
 
 updateBranch="auto-updates"
 echo "how about just git status?"
